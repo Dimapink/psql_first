2a8bd957d57b483f70672ac46f86ab5d2a444651
Dimapink <94777779+Dimapink@users.noreply.github.com>
2024-04-23 06:24:50 +0300

    Update first_task.sql

--- first_task.sql
+++ first_task.sql
@@ -1,36 +1,44 @@
 CREATE SCHEMA music;
 CREATE TABLE IF NOT EXISTS music.genres (
-	id SERIAL PRIMARY KEY, 
-	name VARCHAR(255) UNIQUE NOT NULL
-	);
+  id SERIAL PRIMARY KEY, 
+  name VARCHAR(255) NOT NULL UNIQUE
+  );
 
 CREATE TABLE IF NOT EXISTS music.artist (
-	id SERIAL PRIMARY KEY, 
-	name VARCHAR(255) NOT NULL
-	);
+  id SERIAL PRIMARY KEY, 
+  name VARCHAR(255) NOT NULL UNIQUE
+  );
 
 CREATE TABLE IF NOT EXISTS music.genre_to_artist(
-	genre_id INTEGER REFERENCES music.genres(id),
-	artist_id INTEGER REFERENCES music.artist(id),
-	CONSTRAINT pk PRIMARY KEY (genre_id, artist_id)
-	);
-
+  genre_id INTEGER REFERENCES music.genres(id),
+  artist_id INTEGER REFERENCES music.artist(id)
+  );
 
 CREATE TABLE IF NOT EXISTS music.album (
-	id SERIAL PRIMARY KEY, 
-	name VARCHAR(255) NOT NULL,
-	published DATE NOT NULL
-	);
+  id SERIAL PRIMARY KEY, 
+  name VARCHAR(255) NOT NULL,
+  published DATE NOT NULL CHECK (published BETWEEN '1900-01-01' and now())
+  );
 
 CREATE TABLE IF NOT EXISTS music.artist_to_album(
-	artist_id INTEGER REFERENCES music.artist(id),
-	album_id INTEGER REFERENCES music.album(id),
-	CONSTRAINT fk PRIMARY KEY (artist_id, album_id)
-	);
+  artist_id INTEGER REFERENCES music.artist(id),
+  album_id INTEGER REFERENCES music.album(id)
+  );
 
 CREATE TABLE IF NOT EXISTS music.song (
-	id SERIAL PRIMARY KEY, 
-	name VARCHAR(255) NOT NULL,
-	duration TIME NOT NULL,
-	album INTEGER REFERENCES music.album(id)
-	);
+  id SERIAL PRIMARY KEY, 
+  name VARCHAR(255) NOT NULL,
+  duration INTEGER NOT NULL CHECK (duration BETWEEN 0 and 99999),
+  album INTEGER REFERENCES music.album(id)
+  );
+
+CREATE TABLE IF NOT EXISTS music.collection (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  published DATE NOT NULL CHECK (published BETWEEN '1900-01-01' and now())
+  ); 
+
+CREATE TABLE IF NOT EXISTS music.song_to_collection(
+  collection_id INTEGER REFERENCES music.collection(id),
+  song_id INTEGER REFERENCES music.song(id)
+  );
